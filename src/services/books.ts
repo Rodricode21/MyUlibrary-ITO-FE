32a8708d68cde19cda0004a89a1f0a0f847c20f9
rodrigo serrano <kiradex21@gmail.com>
2024-05-24 12:32:57 -0600

    services

--- src/services/books.ts
+++ src/services/books.ts
@@ -1,8 +1,17 @@
 import { Http } from "@/fetchActions.ts/Http";
-import { Book } from "@/fetchActions.ts/useBooks";
+import { BookQueryParams } from "@/fetchActions.ts/useBooks";
+import { Values } from "@/pages/books/create";
 
 export default {
-  getBooks() {
-    return Http.get("/books");
+  getBooks(params: BookQueryParams) {
+    return Http.get(
+      `/books?genre=${params.genre}&author=${params.author}&title=${params.title}`
+    );
+  },
+  createBooks(body: Values) {
+    return Http.post(`/books/create`, body);
+  },
+  getBookById(id: string) {
+    return Http.get(`/book/${id}`);
   },
 };
